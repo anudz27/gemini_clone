a00cda8caa1fd3c44dcb78ffc67186d06672fe74
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2024-10-07 23:40:29 +0530

    initial

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Sidebar.css'; // Optional: For custom styling
 import menuIcon from '../../assets/menu.png';
+import closeIcon from '../../assets/close.png';
+
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      
-      <img src={menuIcon} alt="Menu Icon" className="sidebar-menu-icon" />
+      <div className='top'>
+        <img className="icon menu" src={menuIcon} alt="Menu Icon" />
+        <div className="new-chat">
+          <img className="icon close" src={closeIcon}/>
+          <p>New Chat</p>
+        </div>
+      </div>
+      <div className='bottom'>
+
+      </div>
       
     </div>
   );
