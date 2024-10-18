916127b45d543f83152a4a34d6002902aa01ec34
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2024-10-18 19:41:29 +0530

    side bar

--- src/components/main/main_side.jsx
+++ src/components/main/main_side.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './main_side.css';
 
 import User from '../../assets/man.png';
+import Message from '../../assets/message.png';
+import Compass from '../../assets/compass.png';
+import Code from '../../assets/code.png';
+import Bulb from '../../assets/bulb.png';
 
 
 const Main = () => {
@@ -16,7 +20,31 @@ const Main = () => {
             <img className="icon" src={User} alt="User"/>
             
             </div>
-           
+            <div className='main-container'>
+                <div className='greet'>
+                    <p><span>Hello,John.</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className='cards'>
+                    <div className='card'>
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img className="compass " src={Compass}/>
+                    </div>
+                    <div className='card'>
+                        <p>Briefly summarize this concept: urban planning</p>
+                        <img className="message " src={Message}/>
+                    </div>
+                    <div className='card'>
+                        <p>Suggestion for breakfast</p>
+                        <img className="bulb " src={Bulb}/>
+                    </div>
+                    <div className='card'>
+                        <p>improve security in notion webapp</p>
+                        <img className="code " src={Code}/>
+                    </div>
+                </div>
+
+            </div>
             
         </div>
     );
